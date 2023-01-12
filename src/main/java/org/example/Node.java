73b0baca19d9707309aa130f18d252102a0484b2
joao victor <joaosan17017vic@gmail.com>
2023-01-12 11:39:33 -0300

    linked list

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,62 @@
+package org.example;
+
+public class Node
+{
+    Node next;
+    int data;
+
+    public Node(int data)
+    {
+        this.data = data;
+    }
+
+}
+
+class LinkedList
+{
+    Node head;
+    void append(int data)
+    {
+        if(head == null)
+        {
+            head = new Node(data);
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null)
+        {
+            current = current.next;
+        }
+
+        current.next = new Node(data);
+
+    }
+
+    void aprepend(int data)
+    {
+        Node newHead = new Node(data);
+        newHead.next = head;
+        head = newHead;
+    }
+
+    void deleteWithData(int data)
+    {
+        if(head == null) return;
+
+        if (head.data == data)
+        {
+            head = head.next;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.data == data) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+}
